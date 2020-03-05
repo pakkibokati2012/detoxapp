5bc048ea6563ba4929a98e28d2b1adcf15c2d1e5
Prakash Bokati <pakkibokati2012@gmail.com>
2020-03-05 11:42:05 +0545

    before githbu

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -8,12 +8,6 @@ describe('Example', () => {
   });
 
   it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+    await expect(element(by.text('See Your Changes'))).toBeVisible();
   });
 });
